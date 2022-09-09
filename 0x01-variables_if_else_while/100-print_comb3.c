a02dd79875ccf7de6fe748e2e8f138e464809530
Busingye Samantha <kedress2112@gmail.com>
2022-09-09 14:24:28 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - print single digit numbers starting from 0
  *
- * Return: returns zero at the end
+ *  Return: returns zero at the end
  */
 int main(void)
 {
@@ -11,22 +11,37 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		
 		for (j = 1; j <= 9; j++)
 		{
+
 			if (j > i)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
 
 			}
 
 		}
-	
+
 	}
-	
+
 	putchar('\n');
 
 	return (0);
 
 }
+
+
+
+
+
+
 
