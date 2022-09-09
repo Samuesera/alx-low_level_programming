3417b1e99b0a5b07aa0ea895e2206f8b4e5ae7de
Busingye Samantha <kedress2112@gmail.com>
2022-09-09 15:34:21 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,13 @@ int main(void)
 	{
 		putchar(c);
 
-	}
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
-	if (c != '9')
-	{
-		putchar(',');
-		putchar(' ');
 	}
-
 	putchar('\n');
 
 	return (0);
