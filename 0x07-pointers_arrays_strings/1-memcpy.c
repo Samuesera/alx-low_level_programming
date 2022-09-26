22ddb57b57826ecd5e4c287466dafb935a7e20ac
Busingye Samantha <kedress2112@gmail.com>
2022-09-26 10:27:45 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/***
+/**
  * _memcpy -  copies memory area from src to dest
  * @dest: destination
  * @src: source
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	 for (i = 0; n > 0 ; i++, n--)
+	for (i = 0; n > 0 ; i++, n--)
 	{
 		dest[i] = src[i];
 	}
